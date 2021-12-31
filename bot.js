c8df6a4d4e939617f04f8d823be90b8df15ed18a
MaxSMoke-Official <maxsmokeskaaj@gmail.com>
2021-12-31 11:54:13 +1000

    Тестовая версия, не работает

--- bot.js
+++ bot.js
@@ -1,37 +1,18 @@
-//инит зависимостей
-const { exec } = require('child_process');
 const { Client, Intents } = require('discord.js');
 const robot = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const si = require('systeminformation');
-const os = require('os-utils');
-let config = require('./json/config.json');
+let config = require('./../json/dsconf.json');
+let utils = require('./lib/utils.js');
+
+let cpuLoad = utils.cpuLoad
+let usedMem = utils.usedMem
+let totalMem = utils.totalMem
 let token = config.token;
 
-//активация бота
 robot.on("ready", function() {
  console.log(robot.user.username + " is running");
 });
 
-//предобьявление переменных
-let cpu, totalmem, usedmem;
-
 setInterval( () => {
-
- //запрашиваем  загруженность процессора
- os.cpuUsage( (v) => {
-  cpu = (v * 100).toFixed(1);
- });
-
- //сбор загруженности озу
- async function getMem() {
-  return (await si.mem());
- };
- getMem().then(x=> {
-  totalmem = (x.total / 1024 / 1024 / 1024).toFixed(1);
-  usedmem = (x.active / 1024 / 1024 / 1024).toFixed(1);
- });
-
- //ставим боту статус
- robot.user.setActivity(`CPU: ${cpu}%, Mem:${usedmem}/${totalmem}GB`, {type: 3});
+ robot.user.setActivity(`CPU: ${cpuLoad}%, Mem:${usedMem}/${totalMem}GB`, {type: 3});
 }, 5000);
-robot.login(token); // аутенфикация по токену
+robot.login(token);
