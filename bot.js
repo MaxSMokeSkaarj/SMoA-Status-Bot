37ac3d8fb93ea4836a09f3b488cbb0072a6bb570
MaxSMoke-Official <maxsmokeskaaj@gmail.com>
2022-01-05 23:43:19 +1000

    Реализация Модульности от Дебрского

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
 const { Client, Intents } = require('discord.js');
 const robot = new Client({ intents: [Intents.FLAGS.GUILDS] });
-let config = require('./../json/dsconf.json');
-let utils = require('./lib/utils.js');
-
-let cpuLoad = utils.cpuLoad
-let usedMem = utils.usedMem
-let totalMem = utils.totalMem
-let token = config.token;
+const config = require('./json/config.json');
+const {getStatus} = require('./lib/utils.js');
+const token = config.token;
 
 robot.on("ready", function() {
- console.log(robot.user.username + " is running");
+	console.log(robot.user.username + " is running");
+	setInterval(() => {
+		getStatus()
+		.then(({cpu, usedMem, totalMem}) => {
+			 robot.user.setActivity(`CPU: ${cpu}%, Mem:${usedMem}/${totalMem}GB`, {type: 3});
+		}).catch(e => {
+			console.log(e);
+		});
+	}, 1000);
+ 
 });
 
-setInterval( () => {
- robot.user.setActivity(`CPU: ${cpuLoad}%, Mem:${usedMem}/${totalMem}GB`, {type: 3});
-}, 5000);
 robot.login(token);
