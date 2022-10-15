1dfa6e276e0779b7e02f78d078b742d9c828c5bb
MaxSMokeSkaarj <maxsmokeskaaj@gmail.com>
2022-10-15 23:16:13 +1000

    Рефакторинг

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 const { Client, Intents } = require('discord.js');
-const robot = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const robot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require('./json/config.json');
-const {getStatus} = require('./lib/utils.js');
-
+const {getStatus, execute} = require('./lib/utils.js');
+let prefix = "!"
 robot.on("ready", function() {
 	console.log(robot.user.username + " is running");
 	setInterval(() => {
@@ -18,4 +18,12 @@ robot.on("ready", function() {
  
 });
 
+robot.on('messageCreate', (msg) => {
+	if (msg.author.username != robot.user.username && msg.author.discriminator != robot.user.discriminator && msg.author.id == 483833827563798552) {
+		let comm = msg.content.trim() + " ";
+		let comm_name = comm.slice(0, comm.indexOf(" "));
+		let messArr = comm.split(" ");
+		if(comm_name == "!exec") {execute(msg, messArr)}
+	} else if (msg.author.id == 483833827563798552) msg.channel.send("Access Denied!")
+})
 robot.login(process.env.DISCORD_TOKEN);
